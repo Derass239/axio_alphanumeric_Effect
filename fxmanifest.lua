df05867c2852217b441c657b88988ee43c41f50d
Valentin Limagne <33382948+Derass239@users.noreply.github.com>
2023-12-08 18:38:23 +0100

    Add code

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+-------------------------------------------------
+------ axio_alphanumeric_Effect by Derass -------
+------------- Discord : Derass#4974 -------------
+--------- https://discord.gg/HPD35pasA5 ---------
+-------------------------------------------------
+
+fx_version("cerulean")
+lua54("yes")
+game("gta5")
+
+name("axio_alphanumeric_Effect")
+author("derass#4974")
+description("Display Text or Number with effect")
+version("1.0.0")
+
+client_script("client/letter.lua")
